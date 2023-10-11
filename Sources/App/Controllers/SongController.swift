47a5c43d0e2db1dcfec012214ca7009e5984e34a
sohee120 <yuhsohee120@gmail.com>
2023-10-11 23:26:39 +0900

    Add: SongController

--- Sources/App/Controllers/SongController.swift
+++ Sources/App/Controllers/SongController.swift
@@ -5,4 +5,18 @@
 //  Created by 윤소희 on 2023/10/10.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+struct SongController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        let songs = routes.grouped("songs")
+        songs.get(use: index)
+    }
+    
+    //song route
+    func index(req: Request) throws -> EventLoopFuture<[Song]> {
+        return Song.query(on: req.db).all()
+    }
+    
+}
