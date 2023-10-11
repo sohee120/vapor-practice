dfe3b667deeacc0ff02ccf74f749bac258f31b52
sohee120 <yuhsohee120@gmail.com>
2023-10-11 23:27:37 +0900

    Add: Song(Model)

--- Sources/App/Models/Song.swift
+++ Sources/App/Models/Song.swift
@@ -1,8 +1,26 @@
 //
-//  File.swift
+//  Song.swift
 //  
 //
 //  Created by 윤소희 on 2023/10/10.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+final class Song: Model, Content {
+    static let schema = "songs"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    init() {}
+    
+    init(id: UUID? = nil, title: String) {
+        self.id = id
+        self.title = title
+    }
+}
