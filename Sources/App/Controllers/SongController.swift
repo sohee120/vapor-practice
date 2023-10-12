743844d4659e72c9e72ebc0b5e8d929b3e7aee9a
sohee120 <yuhsohee120@gmail.com>
2023-10-12 19:27:31 +0900

    Feat: post메서드 create함수 작성

--- Sources/App/Controllers/SongController.swift
+++ Sources/App/Controllers/SongController.swift
@@ -12,11 +12,17 @@ struct SongController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let songs = routes.grouped("songs")
         songs.get(use: index)
+        songs.post(use: create)
     }
     
     //song route
     func index(req: Request) throws -> EventLoopFuture<[Song]> {
         return Song.query(on: req.db).all()
+    }
+    
+    func create(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let song = try req.content.decode(Song.self)
+        return song.save(on: req.db).transform(to: .ok)
     }
     
 }
